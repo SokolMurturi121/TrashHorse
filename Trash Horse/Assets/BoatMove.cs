dd03877278b16455ed83fa39f48a3687d643e088
Rory <rd553@york.ac.uk>
2017-01-21 16:42:23 +0000

    Simple wave physics

---
+++ Trash Horse/Assets/BoatMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float x = gameObject.GetComponent<Rigidbody2D> ().velocity.x;
+		float y = gameObject.GetComponent<Rigidbody2D> ().velocity.y;
+		//if (x ^ 2 + y ^ 2 > 10) {
+		//GameObject w = Instantiate(Resources.Load("waves")) as GameObject;
+		//w.transform.position = gameObject.transform.position;
+		//}
+
+		if (Input.GetKeyDown (KeyCode.W)) {
+			this.gameObject.GetComponent<Rigidbody2D> ().AddForce (gameObject.transform.right * 60);
+		}
+
+		if (Input.GetKeyDown (KeyCode.A)) {
+			gameObject.GetComponent<Rigidbody2D> ().AddTorque (2f);
+		}
+
+		if (Input.GetKeyDown (KeyCode.D)) {
+			gameObject.GetComponent<Rigidbody2D> ().AddTorque (-2f);
+
+		}
+
+		//gameObject.GetComponent<Rigidbody2D>().velocity
+
+
+	}
+
+}
---
+++ Trash Horse/Assets/Wake.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wake : MonoBehaviour {
+
+	public string string_dir;
+	public float force;
+	private Vector2 vector_dir;
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D col){
+
+		switch (string_dir) {
+		case "up":
+			vector_dir = transform.up;
+			break;
+		case "down":
+			vector_dir = -transform.up;
+			break;
+		case "back":
+			vector_dir = -transform.right;
+			break;
+		default:
+			vector_dir = transform.up;
+			break;
+		}
+
+		Vector2 dir = (col.gameObject.transform.position - gameObject.transform.position);
+		float dist = Mathf.Sqrt(dir.x*dir.x + dir.y*dir.y);
+
+		if(dist>0) col.gameObject.GetComponent<Rigidbody2D> ().AddForce(vector_dir*force*1/dist);
+
+			//(new Vector2(dir.x*2*1/dist,dir.y*2*1/dist));
+
+	}
+
+
+}
