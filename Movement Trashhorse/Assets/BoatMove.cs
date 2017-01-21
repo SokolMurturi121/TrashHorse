c25f4e71ad2632b99b743588710916ee35008a43
Rory <rd553@york.ac.uk>
2017-01-21 19:58:46 +0000

    changed some movement physics

---
+++ Movement Trashhorse/Assets/BoatMove.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMove : MonoBehaviour {
+
+	public float torque;
+	public float topspeed;
+	public float toptorque;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 vel = gameObject.GetComponent<Rigidbody2D> ().velocity;
+		float speed = Mathf.Sqrt (vel.x * vel.x + vel.y * vel.y);
+		Vector3 pos = gameObject.transform.position;
+
+
+
+
+	
+
+		if (Input.GetKey (KeyCode.W)) {
+			this.gameObject.GetComponent<Rigidbody2D> ().AddForce (gameObject.transform.right * 10);
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			gameObject.GetComponent<Rigidbody2D> ().AddTorque (torque);
+		}
+
+		if (Input.GetKey(KeyCode.D)) {
+			gameObject.GetComponent<Rigidbody2D> ().AddTorque (-torque);
+
+		}
+
+		vel.Normalize ();
+
+		gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (transform.right.x * speed, transform.right.y * speed);
+		if (speed > topspeed) {
+			Vector2 newvel = new Vector2 (topspeed * vel.x, topspeed * vel.y);
+			gameObject.GetComponent<Rigidbody2D> ().velocity = newvel;
+
+		}
+		//gameObject.GetComponent<Rigidbody2D>().velocity
+
+
+	}
+
+}
---
+++ Movement Trashhorse/Assets/WrapBack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrapBack : MonoBehaviour {
+
+	public bool x;
+	public bool y;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.gameObject.layer == 8 || col.gameObject.layer == 9) {
+			Vector2 pos = col.gameObject.transform.position;
+			col.gameObject.transform.position = new Vector2 (x ? (pos.x<0?9.1f:-9.1f) : pos.x, y ?  (pos.y<0?5.1f:-5.1f) : pos.y);
+
+
+		}
+	}
+}
