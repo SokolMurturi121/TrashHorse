f59fafe7b49fff990b9057941fdf60b106bcdb6e
Rory <rd553@york.ac.uk>
2017-01-21 12:49:59 +0000

    Controls or something

---
+++ Boats game thing/Assets/BoatMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.W)) {
+			this.gameObject.GetComponent<Rigidbody2D> ().AddForce (gameObject.transform.right * 60);
+			Debug.Log ("hi");
+		}
+
+		if (Input.GetKeyDown (KeyCode.A)) {
+			gameObject.GetComponent<Rigidbody2D> ().AddTorque (1f);
+		}
+
+		if (Input.GetKeyDown (KeyCode.E)) {
+			gameObject.GetComponent<Rigidbody2D> ().AddTorque (-1f);
+
+		}
+
+		//gameObject.GetComponent<Rigidbody2D>().velocity
+
+
+	}
+
+}
---
+++ Boats game thing/Assets/Wave.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 s = gameObject.transform.localScale;
+
+		gameObject.transform.localScale = new Vector3 (s.x * 1.02f, s.x * 1.02f, s.z * 1.02f);
+	}
+}
